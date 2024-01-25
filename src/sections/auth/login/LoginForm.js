febdf4cea194d5cc970794698f90f5612096880c
jainl28patel <jainilpatel2003@gmail.com>
2024-01-25 13:00:01 +0530

    auth: integrate with backend

--- src/sections/auth/login/LoginForm.js
+++ src/sections/auth/login/LoginForm.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from "axios";
 import { useNavigate } from 'react-router-dom';
 // @mui
 import { Link, Stack, IconButton, InputAdornment, TextField, Checkbox } from '@mui/material';
@@ -11,20 +12,65 @@ import Iconify from '../../../components/iconify';
 export default function LoginForm() {
   const navigate = useNavigate();
 
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [Password, setPassword] = useState("");
+
+  useEffect(() => {
+    // check if user is logged in
+    axios.get('/check-already-login').then((res) => {
+      console.log(res.data);
+      if(res.data.status == "success"){
+        navigate('/dashboard/app', { replace: true });
+      }
+    }
+    ).catch((err) => {
+      console.log(err);
+    })
+  }, []);
   
 
   const handleClick = () => {
-    if(Password == "admin"){
-    navigate('/dashboard/app', { replace: true });
+    // if(Password == "admin"){
+    // navigate('/dashboard/app', { replace: true });
+    // }
+    if(username == "" || email == "" || Password == ""){
+      alert("Please fill all the fields");
+      return;
     }
+
+    try {
+      axios.post('/admin-login', {
+        name: username,
+        email: email,
+        password: Password
+      }).then((res) => {
+        console.log(res.data);
+        if(res.data.status == "success"){
+          navigate('/dashboard/app', { replace: true });
+        }
+        else{
+          alert("Invalid credentials");
+        }
+      }
+      ).catch((err) => {
+        console.log(err);
+        alert("Invalid credentials");
+      })  
+    } catch (error) {
+      alert("Invalid credentials");
+    }
+
   };
 
   return (
     <>
       <Stack spacing={3}>
-        <TextField name="email" label="Email address" />
+
+        <TextField name="username" label="Username" onChange={(e) => setUsername(e.target.value)} />
+
+        <TextField name="email" label="Email address" onChange={(e) => setEmail(e.target.value)} />
 
         <TextField
           name="password"
